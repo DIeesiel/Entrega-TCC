c082d135c49b5dba9017594bbcbe2d6416ad731f
Mateus da Rosa Elias <mateuselias4048@gmail.com>
2023-09-29 11:59:21 -0300

    Menu +- resolvido

--- menuLateral/dizimistas.php
+++ menuLateral/dizimistas.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <style>
 h1 {
@@ -20,39 +20,43 @@ font-weight: bold;
 
 <h1>Cadastro do Dizimista</h1>
 <form method="post" action="../importacao2.php">
-    <div class="form-group">
+    <div class="col-12">
       <label for="inputAddress">Nome</label>
       <input type="text" name="nome" class="form-control" id="inputAddress" placeholder="Jacinto Aberlado" required>
     </div>
-      <div class="form-group">
+</br>
+      <div class="col-12">
         <label for="inputCity">Endereço</label>
         <input type="text" name="endereco" class="form-control" id="inputCity" placeholder="Rua dos Bobos, nº 0" required>
       </div>
-    <div class="form-row">
-      <div class="form-group col-6">
+</br>
+    <div class="row g-3">
+      <div class="col-6">
         <label for="inputCPF">CPF</label>
         <input type="text" name="cpf" class="form-control" id="inputEmail4" placeholder="xxx.xxx.xxx-xx" maxlength="14" required>
       </div>
-      <div class="form-group col-6">
+      <div class="col-6">
         <label for="inputPassword4">Telefone</label>
         <input type="text" name="telefone" class="form-control" id="inputPassword4" placeholder="(xx) xxxxx-xxxx" required>
       </div>
     </div>
-    <div class="form-row">
-      <div class="form-group col-6">
+</br>
+    <div class="row g-3">
+      <div class="col-sm-6">
         <label for="inputCity">Coordenador</label>
         <input type="text" name="coordenador" class="form-control" id="inputCity" placeholder="Maria Aparecida" required>
       </div>
-      <div class="form-group col-4">
+      <div class="col-sm-4">
         <label for="inputEstado">Valor</label>
         <input type="text" name="valor" class="form-control" id="inputCEP" placeholder="R$" required>
       </div>
-      <div class="form-group col-2">
+      <div class="col-sm-2">
         <label for="inputCEP">Cod. Dizimo</label>
         <input type="text" name="codigo" class="form-control" id="inputCEP" maxlength="4" placeholder="xxxx" required>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary" class="butao">Marcar</button>
+</br>
+    <button type="submit" class="btn btn-primary">Marcar</button>
   </form>
 </div>
 </main>
